c71914e7dd36ee59272ac7fca99a054d0ccbed67
Simran Nayak <simrannayak763@gmail.com>
2024-08-23 12:35:25 +0530

    Create comaparison.js

---
+++ Operator in js/comaparison.js
@@ -0,0 +1,16 @@
+let str1 = "hello";
+let str2 = "world";
+if (str1 == str2) {
+  console.log("string are equal");
+} else {
+  console.log("string are not equal");
+}
+//Comparison operator include 
+/* 
+Equal to ==
+Equal value and equal type ===
+Strict not equal !==
+Greater than >
+Greater than or equal to >=
+Less than <
+Less than equal to <=
