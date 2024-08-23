e4fdc0d1ce666fa8aafeb73c99bd696364943193
Simran Nayak <simrannayak763@gmail.com>
2024-08-23 12:26:34 +0530

    Add files via upload

---
+++ Operator in js/arithmetic .js
@@ -0,0 +1,15 @@
+let n1 = 120;
+let n2 = 50;
+console.log("addition : " + (n1 + n2));
+console.log("Subtraction:" + (n1 - n2));
+console.log("Multiplication :" + n1 * n2);
+console.log("Division : " + n1 / n2);
+console.log("Module : " + (n1 % n2));
+
+/* Arithmetic operator involves operator like
+additon
+subtraction
+multiplication
+division
+Modulus operator involves operator like
+remainder*/
